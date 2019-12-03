461a77ad48e661c85a9e499c69277a998a4fc293
Edward Plato <edplatomail@gmail.com>
2019-12-03 10:31:29 -0500

    fix bad sub

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
-githubRepo="https://github.com/${$GITHUB_REPOSITORY}"
-echo "$githubRepo"
+githubRepo="https://github.com/$GITHUB_REPOSITORY"
+echo $githubRepo
 trufflehog --regex --entropy=True $githubRepo
