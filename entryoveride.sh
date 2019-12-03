fc799049339589b52995f553f6786cd01beb6f52
Edward Plato <edplatomail@gmail.com>
2019-12-03 09:50:10 -0500

    remove entropy false option

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
-trufflehog --regex --entropy=False --since_commit $GITHUB_SHA $GITHUB_WORKSPACE
+trufflehog --regex --since_commit $GITHUB_SHA $GITHUB_WORKSPACE
