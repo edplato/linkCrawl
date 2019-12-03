f240444ea872f0f533248f8c8076b7246c1e2e15
Edward Plato <edplatomail@gmail.com>
2019-12-03 10:40:55 -0500

    test max depth

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
 githubRepo="https://github.com/$GITHUB_REPOSITORY"
-trufflehog --regex --entropy=False --since_commit $GITHUB_SHA $githubRepo
+trufflehog --regex --entropy=False --max_depth=5 $githubRepo
