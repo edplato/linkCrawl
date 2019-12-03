e4a60cd6782d0fda3b53df6213799cc62468446b
Edward Plato <edplatomail@gmail.com>
2019-12-03 10:03:24 -0500

    revert th test

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
-th=trufflehog --regex --entropy DO_ENTROPY $GITHUB_WORKSPACE
-echo "$th"
+trufflehog --regex --entropy DO_ENTROPY $GITHUB_WORKSPACE
