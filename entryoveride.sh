aca001857865fde4f26bd07deadb1dd1763b4e4f
Edward Plato <edplatomail@gmail.com>
2019-12-03 09:31:38 -0500

    remove options

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,7 +1,3 @@
-#!/bin/bash -l
-
+#!/usr/bin/env sh
 set -e # Abort script at first error
-set -u # nounset - Attempt to use undefined variable outputs error message, and forces an exit
-# set -x # verbose (expands commands)
-
-trufflehog --regex --entropy=False $GITHUB_WORKSPACE
+trufflehog $GITHUB_WORKSPACE
