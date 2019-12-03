08fa9fc9a53f1a5dafe432a614e3274bd96eed47
Edward Plato <edplatomail@gmail.com>
2019-12-03 09:23:59 -0500

    change overide to use trufflehog test

--- entryoveride.sh
+++ entryoveride.sh
@@ -4,7 +4,4 @@ set -e # Abort script at first error
 set -u # nounset - Attempt to use undefined variable outputs error message, and forces an exit
 # set -x # verbose (expands commands)
 
-gitleaks -v --redact --threads=1 \
-  --branch=$GITHUB_REF \
-  --repo-path=$GITHUB_WORKSPACE \
-  --commit=$GITHUB_SHA \
+trufflehog --regex --entropy=False --since_commit $GITHUB_SHA $GITHUB_WORKSPACE
