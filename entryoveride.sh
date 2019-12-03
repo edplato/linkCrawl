45a829af5fe2e204a55d342525e5f602e9845bb6
Edward Plato <edplatomail@gmail.com>
2019-12-03 10:38:13 -0500

    key test

--- entryoveride.sh
+++ entryoveride.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
 githubRepo="https://github.com/$GITHUB_REPOSITORY"
-echo $githubRepo
 trufflehog --regex --entropy=False --since_commit $GITHUB_SHA $githubRepo
